aa0959f80cd53bb930f43e4b68482e85aea1ead8
Dr-MAhmed <dr.muhammadahmed05@gmail.com>
2024-03-04 09:20:07 +0500

    Formula Explanation

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let btn = document.getElementById("btn");
 let arr = Array.from(boxes);
 
 function getRandomColor() {
-  let val1 = Math.ceil(0 + Math.random() * 255);
-  let val2 = Math.ceil(0 + Math.random() * 255);
-  let val3 = Math.ceil(0 + Math.random() * 255);
+  let val1 = Math.ceil(0 + Math.random() * (255 - 0));
+  let val2 = Math.ceil(0 + Math.random() * (255 - 0));
+  let val3 = Math.ceil(0 + Math.random() * (255 - 0));
      return `rgb(${val1}, ${val2}, ${val3})`;
 }
 
@@ -15,3 +15,9 @@ btn.addEventListener("click", () => {
     e.style.color = getRandomColor();
   });
 });
+
+// Formula Explanation.
+// a, b (b - a);
+// a + r (b - a);
+// 0 + Math.random() * (255 - 0);
+// Math.ceil(0 + Math.random() * 255)
